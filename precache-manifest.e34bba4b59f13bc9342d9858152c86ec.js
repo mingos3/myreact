560d67590e59d360dc3f47ffea376a6b1c7cf9cf
mingos3 <ramamatodomingo@gmail.com>
2019-02-26 17:25:20 +0100

    Updates

---
+++ precache-manifest.e34bba4b59f13bc9342d9858152c86ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e01a2b72839c566ee4f4",
+    "url": "/myreact/static/js/runtime~main.e01a2b72.js"
+  },
+  {
+    "revision": "6a4257e387ce303623bd",
+    "url": "/myreact/static/js/main.6a4257e3.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/myreact/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "6a4257e387ce303623bd",
+    "url": "/myreact/static/css/main.22523d87.chunk.css"
+  },
+  {
+    "revision": "8608509bca5080145025c67abb70011c",
+    "url": "/myreact/index.html"
+  }
+];
